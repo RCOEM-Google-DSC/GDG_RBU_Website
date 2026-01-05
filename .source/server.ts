e3e1b271c5bdaa602846ae8d72bbcd00740cb489
Saptanshu <saptanshuwanjari63@gmail.com>
2026-01-05 19:50:57 +0530

    feat: starter github docs

--- .source/server.ts
+++ .source/server.ts
@@ -1,10 +1,11 @@
 // @ts-nocheck
-import * as __fd_glob_10 from "../content/docs/starter.mdx?collection=docs"
-import * as __fd_glob_9 from "../content/docs/index.mdx?collection=docs"
-import * as __fd_glob_8 from "../content/docs/idea-submission-form.mdx?collection=docs"
-import * as __fd_glob_7 from "../content/docs/google-maps.mdx?collection=docs"
-import * as __fd_glob_6 from "../content/docs/google-cloud.mdx?collection=docs"
-import * as __fd_glob_5 from "../content/docs/google-auth.mdx?collection=docs"
+import * as __fd_glob_11 from "../content/docs/starter.mdx?collection=docs"
+import * as __fd_glob_10 from "../content/docs/index.mdx?collection=docs"
+import * as __fd_glob_9 from "../content/docs/idea-submission-form.mdx?collection=docs"
+import * as __fd_glob_8 from "../content/docs/google-maps.mdx?collection=docs"
+import * as __fd_glob_7 from "../content/docs/google-cloud.mdx?collection=docs"
+import * as __fd_glob_6 from "../content/docs/google-auth.mdx?collection=docs"
+import * as __fd_glob_5 from "../content/docs/github-setup.mdx?collection=docs"
 import * as __fd_glob_4 from "../content/docs/getting-started.mdx?collection=docs"
 import * as __fd_glob_3 from "../content/docs/gemini.mdx?collection=docs"
 import * as __fd_glob_2 from "../content/docs/firebase.mdx?collection=docs"
@@ -18,4 +19,4 @@ const create = server<typeof Config, import("fumadocs-mdx/runtime/types").Intern
   }
 }>({"doc":{"passthroughs":["extractedReferences"]}});
 
-export const docs = await create.docs("docs", "content/docs", {"meta.json": __fd_glob_0, }, {"clone-starter-repo.mdx": __fd_glob_1, "firebase.mdx": __fd_glob_2, "gemini.mdx": __fd_glob_3, "getting-started.mdx": __fd_glob_4, "google-auth.mdx": __fd_glob_5, "google-cloud.mdx": __fd_glob_6, "google-maps.mdx": __fd_glob_7, "idea-submission-form.mdx": __fd_glob_8, "index.mdx": __fd_glob_9, "starter.mdx": __fd_glob_10, });
+export const docs = await create.docs("docs", "content/docs", {"meta.json": __fd_glob_0, }, {"clone-starter-repo.mdx": __fd_glob_1, "firebase.mdx": __fd_glob_2, "gemini.mdx": __fd_glob_3, "getting-started.mdx": __fd_glob_4, "github-setup.mdx": __fd_glob_5, "google-auth.mdx": __fd_glob_6, "google-cloud.mdx": __fd_glob_7, "google-maps.mdx": __fd_glob_8, "idea-submission-form.mdx": __fd_glob_9, "index.mdx": __fd_glob_10, "starter.mdx": __fd_glob_11, });
